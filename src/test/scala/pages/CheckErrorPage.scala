346c4e52579ec0a45a0f86e71ae818e7f82e80bd
Kuba-original <kuba.a.jablonowski@capgemini.com>
2025-09-18 14:39:47 +0100

    UI Test 2

---
+++ src/test/scala/pages/CheckErrorPage.scala
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ */
+
+package pages
+
+object CheckErrorPage extends BasePage {
+
+
+}
--- src/test/scala/specs/RegistrationJourneySpec.scala
+++ src/test/scala/specs/RegistrationJourneySpec.scala
@@ -87,5 +87,64 @@ class RegistrationJourneySpec extends BaseSpec {
       Given("The user has an error")
       BusinessDatePage.findError shouldBe "There is a problem\nMonth out of range"
     }
+
+    Scenario("A user wants to sign up for Boiled Sweet Tax but business name is already used") {
+
+      Given("The user is on the homepage")
+      IndexPage.navigateTo()
+      IndexPage.pageTitle() shouldBe "Welcome to the Boiled Sweet Tax registration service"
+
+      Then("The user clicks the submit button")
+      IndexPage.submit()
+
+      Given("The user is on the business name page")
+      BusinessNamePage.pageTitle() shouldBe "What is the name of your business?"
+
+      Then("The user clicks the submit button")
+      BusinessNamePage.submit("Already used")
+
+      Given("The user is on the date of business started page")
+      BusinessDatePage.pageTitle() shouldBe "What is the Date that your business started trading?"
+
+      Then("The user clicks the submit button")
+      BusinessDatePage.submit(1, 10, 1990)
+
+      Given("The user is on the business address page")
+      BusinessAddressPage.pageTitle() shouldBe "What is the address of your business?"
+
+      Then("The user clicks the submit button")
+      val testAddress = Address("123", "Test Street", "Test City", "Testshire", "TE5 7ED")
+      BusinessAddressPage.submit(testAddress)
+
+      Given("The user is on the check your answers page")
+      CheckAnswersPage.pageTitle() shouldBe "Check your answers"
+
+      Then("The user clicks the submit button")
+      CheckAnswersPage.submit()
+
+      Given("The user is on the confirmation page")
+      CheckConfirmationPage.pageTitle() shouldBe "Registration Complete"
+
+      Then("The user clicks the submit button")
+      CheckConfirmationPage.submit()
+
+      IndexPage.pageTitle() shouldBe "Welcome to the Boiled Sweet Tax registration service"
+
+      Then("The user clicks the submit button")
+      IndexPage.submit()
+
+      Given("The user is on the business name page")
+      CheckErrorPage.pageTitle() shouldBe "What is the name of your business?"
+
+      Then("The user clicks the submit button")
+      BusinessNamePage.submit("Already used")
+
+      Given("The user is on the name is used error")
+      CheckErrorPage.pageTitle() shouldBe "service error"
+
+
+
+
+
+    }
   }
-}
